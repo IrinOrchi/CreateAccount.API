275a1bc0bed7bb580968539de581a5c487b426b2
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-09-30 16:31:55 +0600

    Update LocationRepository.cs

--- CreateAccount.Repository/Repository/LocationRepository.cs
+++ CreateAccount.Repository/Repository/LocationRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using CreateAccount.AggregateRoot.Entities;
+using CreateAccount.DTO.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,33 +22,46 @@ namespace CreateAccount.Repository.Repository
             _context = context;
         }
 
-        // Retrieve locations based on the districtId and outsideBd flags
         public async Task<List<LocationResponseDTO>> GetLocationsAsync(string districtId, string outsideBd)
         {
-            string query;
+            IQueryable<Location> query = _context.Locations;
 
             if (!string.IsNullOrEmpty(outsideBd) && outsideBd == "1")
             {
-                query = "SELECT L_ID, L_Name FROM Locations WHERE OutsideBangladesh = 1 ORDER BY L_Name";
+                query = query.Where(l => l.OutsideBangladesh == true);
             }
-            else if (string.IsNullOrEmpty(districtId))
+            else if (string.IsNullOrEmpty(districtId)) 
             {
-                query = "SELECT L_ID, L_Name FROM Locations WHERE OutsideBangladesh = 0 and l_Type = 'District' ORDER BY L_Name";
+                query = query.Where(l => l.OutsideBangladesh == false && l.L_Type == "District");
             }
             else
             {
-                query = $"SELECT L_ID, L_Name FROM Locations WHERE OutsideBangladesh = 0 and l_Type = 'thana' and ParentID = {districtId}";
+               
+                if (districtId == "0")
+                {
+        
+                    query = query.Where(l => l.OutsideBangladesh == false && l.L_Type == "District");
+                }
+                else
+                {
+            
+                    int districtIdInt = int.Parse(districtId); 
+                    query = query.Where(l => l.OutsideBangladesh == false && l.L_Type == "thana" && l.ParentID == districtIdInt);
+                }
             }
 
-            // Execute the query and return the result
-            return await _context.Locations
-                .FromSqlRaw(query)
+            query = query.OrderBy(l => l.L_Name);
+
+            
+            return await query
                 .Select(l => new LocationResponseDTO
                 {
                     OptionValue = l.L_ID.ToString(),
                     OptionText = l.L_Name
-                }).ToListAsync();
+                })
+                .ToListAsync();
         }
+
     }
 
 }
