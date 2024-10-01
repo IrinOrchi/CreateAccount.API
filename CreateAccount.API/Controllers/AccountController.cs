2883d2016fa019374b4024bfdfa71ffc87dbaa03
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-10-01 12:20:31 +0600

    Update AccountController.cs

--- CreateAccount.API/Controllers/AccountController.cs
+++ CreateAccount.API/Controllers/AccountController.cs
@@ -28,28 +28,5 @@ namespace CreateAccount.API.Controllers
 
             return Ok(response);
         }
-
-        // NEW: Check if both UserName and CompanyName exist (GET method)
-        [HttpGet("CheckUserAndCompany")]
-        public async Task<IActionResult> CheckUserAndCompany([FromQuery] string userName, [FromQuery] string companyName)
-        {
-            var checkRequest = new CheckNamesRequestDTO
-            {
-                UserName = userName,
-                CompanyName = companyName
-            };
-
-            var response = await _checkNamesHandler.Handle(checkRequest);
-            if (!string.IsNullOrEmpty(response.Message))
-            {
-                return BadRequest(response);
-            }
-
-            return Ok(new
-            {
-                UserExists = string.IsNullOrEmpty(checkRequest.UserName) ? false : await _checkNamesHandler.UserExistsAsync(checkRequest.UserName),
-                CompanyExists = string.IsNullOrEmpty(checkRequest.CompanyName) ? false : await _checkNamesHandler.CompanyExistsAsync(checkRequest.CompanyName)
-            });
-        }
     }
 }
